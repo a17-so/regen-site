f85c9fe72a6bb5b0bd8723cf095a5b8a7922f79f
Abhay Chebium <abhaychebium@gmail.com>
2026-02-23 19:59:29 -0800

    test 1

--- src/app/components/AppStoreButton.tsx
+++ src/app/components/AppStoreButton.tsx
@@ -1,20 +1,21 @@
 import Image from "next/image";
-import Link from "next/link";
 
 export default function AppStoreButton() {
-    return (
-        <Link
-            href="https://apps.apple.com/us/app/hardmaxx-transform-now/id6756548399"
-            className="inline-block transition-transform hover:scale-105 active:scale-95"
-        >
-            <Image
-                src="/images/download.png"
-                alt="Download on the App Store"
-                width={200}
-                height={66}
-                className="h-[60px] w-auto rounded-[18px]"
-                unoptimized
-            />
-        </Link>
-    );
+  return (
+    <a
+      href="https://apps.apple.com/us/app/hardmaxx-transform-now/id6756548399"
+      className="inline-block transition-transform hover:scale-105 active:scale-95"
+      rel="nofollow"
+    >
+      <Image
+        src="/images/download.png"
+        alt="Download on the App Store"
+        width={200}
+        height={66}
+        className="h-[60px] w-auto rounded-[18px]"
+        unoptimized
+        priority
+      />
+    </a>
+  );
 }
