988165a62349ea76653d47219d238d427ef82067
Abhay Chebium <abhaychebium@gmail.com>
2026-03-06 14:08:54 -0800

    form redirect implementation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/ugc",
+        destination:
+          "https://fog-booklet-8f9.notion.site/31b39b5e840f80b6a5a5ff2c13cacfef?pvs=105",
+        permanent: false,
+      },
+      {
+        source: "/ai-influencers",
+        destination:
+          "https://fog-booklet-8f9.notion.site/31b39b5e840f80aba077c81dfceb98d9?pvs=105",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
